e257be06ce65909aec3c94ac45ebd201ad787d5e
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-04-20 09:33:22 +0900

    Reactプロジェクトを作成

---
+++ front/src/Routes.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+
+export const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+        </Routes>
+    )
+};
